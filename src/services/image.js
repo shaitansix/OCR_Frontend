34212a385aff21ebacf6f726d65eee586482a924
shaitansix <shaitansix3@gmail.com>
2025-02-28 11:43:05 -0500

    Cambio de url del servicio de la api

--- src/services/image.js
+++ src/services/image.js
@@ -2,7 +2,7 @@
 // const BASE_URL = 'http://127.0.0.1:8000/image'
 
 /* render */
-const BASE_URL = 'https://ocr-backend-p48p.onrender.com/image'
+const BASE_URL = 'https://ocr-backend-t90p.onrender.com/image'
 const EXTRACT_TEXT_URL = `${BASE_URL}/extract_text`
 
 export const extractText = async (data) => {
